c1998f11d1706532dbe71ee167392f65fb781f05
quizzic <eosqqone@gmail.com>
2021-11-29 13:02:33 +0500

    Выполнение 2 урока

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-<<<<<<< HEAD
-=======
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Калькулятор верстки';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 120;
+let rollback = 40;
+let fullPrice = 250;
+let adaptive = true;
 
-alert ('Hello World!');
-console.log ('Любое сообщение');
->>>>>>> lesson01
+console.log (typeof title);
+console.log (typeof fullPrice);
+console.log (typeof adaptive);
+console.log (screens.length)
+console.log ('Стоимость верстки экранов' + ' ' + screenPrice + ' ' + 'рублей/ долларов/гривен/юани')
+console.log ('Стоимость разработки сайта' + ' ' + fullPrice + ' ' + 'рублей/ долларов/гривен/юани')
+console.log (screens.toLowerCase().split(", "))
+console.log (fullPrice * (rollback/100))
