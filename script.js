16b8eb640ad9b2b4588d0b17aae8710b3f4f0bb3
quizzic <eosqqone@gmail.com>
2021-12-01 15:27:02 +0500

    Исправление усложненного задания к уроку 2

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let num = 266219;
 
 let multiplication = eval(num.toString().split('').join('*')); // 1) конвертация из типа Number в String для сплита;
 // 2) сплит строчного выражения на отдельные элементы массива; 3) перемножение каждого элемента в массиве друг на друга
+console.log(multiplication)
+
 let result = (multiplication **= 3).toString(); // возведение в степень и конвертация в String
 
 console.log (result.slice(0,2)); //Вывод в консоль первых 2 цифр полученного числа
